3296153bbf4d52d1f00010e35633f8ede14e0450
Ali-zaid <az7425328@gmail.com>
2025-02-22 10:07:30 +0500

    first commit

---
+++ po.js
@@ -0,0 +1,12 @@
+"use strict";
+class varii {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+const p1 = new varii(8, 9);
+const p2 = new varii(6, 8);
+console.log(p1);
+console.log(p2);
+console.log(`${p1.x}${p2.y}`); //88
---
+++ po.ts
@@ -0,0 +1,15 @@
+class varii {
+    x:number;
+    y:number;
+    constructor(x:number , y:number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+const p1 = new varii(8,9);
+const p2 = new varii(6,8);
+ console.log(p1);
+ console.log(p2);
+console.log(`${p1.x}${p2.y}`);//88
+
+
